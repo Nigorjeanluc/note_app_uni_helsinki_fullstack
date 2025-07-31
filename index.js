f2762ec85b62c37107b0c78a572d8ec46f490c32
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2025-07-31 10:26:11 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
 const Note = require('./models/note')
 
 const app = express()
@@ -46,14 +45,15 @@ app.get('/api/notes', (request, response) => {
 // })
 
 app.get('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id).then(note => {
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 // app.delete('/api/notes/:id', (request, response) => {
@@ -65,7 +65,7 @@ app.get('/api/notes/:id', (request, response, next) => {
 
 app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
-    .then(result => {
+    .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
@@ -90,19 +90,19 @@ app.put('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
+// const generateId = () => {
+//   const maxId = notes.length > 0
+//     ? Math.max(...notes.map(n => Number(n.id)))
+//     : 0
+//   return String(maxId + 1)
+// }
 
 // app.post('/api/notes', (request, response) => {
 //   const body = request.body
 
 //   if (!body.content) {
-//     return response.status(400).json({ 
-//       error: 'content missing' 
+//     return response.status(400).json({
+//       error: 'content missing'
 //     })
 //   }
 
